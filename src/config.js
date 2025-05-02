f3dd697ef5e3fc39cfe3d24cb2b028e18b8012ec
tommydavis <tommydavis@tommydavisMac-mini.local>
2025-05-02 17:42:54 +0800

    跨域问题 2nd try

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const config = {
     perSecond: 5, // 每秒最多5次请求
     perDay: 2000 // 每天最多2000次请求
   },
-  runeAssetCacheDuration: 30 * 60000, // 符文资产数据缓存时间，单位：分钟
-  runePriceCacheDuration: 1 * 60000 // 符文价格缓存时间，单位：分钟
+  runeAssetCacheDuration: 60 * 60000, // 符文资产数据缓存时间，单位：分钟
+  runePriceCacheDuration: 60 * 60000 // 符文价格缓存时间，单位：分钟
 };
 
 export default config;
